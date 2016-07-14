a18af20aabbe80f930def7f3eac5a5868b14ffb9
Tim Williams <t.james.williams@gmail.com>
2016-07-13 18:51:12 -0700

    bugfix

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ io.on('connection', (socket) => {
 	socket.on('sessions:advanceSession', (data,cb) => {
 		var beers = require(__dirname+'/modules/beers');
 		beers = new beers();
-		beers.nextBeer(data.beer_id,data.session_id, (err,beer) => {
+		beers.nextBeer(data.beer_id,data.session_id, (beer) => {
 			if( err ) throw err;
 			// --> Broadcast the advancement to all users.
 			var route = beer.id ? '/tasting-round/'+beer.id : 'summary';
