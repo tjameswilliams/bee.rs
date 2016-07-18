94f8533af2dc2db73f7f9ef38529b3d5c344b398
Tim Williams <t.james.williams@gmail.com>
2016-07-18 09:03:40 -0700

    run server from gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 let gulp = require('gulp');
 let ts = require('gulp-typescript');
+let nodemon = require('gulp-nodemon');
 
 gulp.task('copy_assets', () => {
 	return gulp.src(['./src/**/*', '!./**/*.ts'])
@@ -14,6 +15,13 @@ gulp.task('compile_ts', () => {
 		.pipe(gulp.dest('dist/app'));
 });
 
-gulp.task('watch', () => {
+gulp.task('start', () => {
+	nodemon({
+		script: 'index.js',
+		ext: 'js',
+  })
+});
+
+gulp.task('watch', ['start'], () => {
 	gulp.watch('src/app/**/*', ['copy_assets','compile_ts']);
 });
